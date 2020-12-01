4fa7555a0eebf72514026bec25c9b0f91f229c76
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-01 07:25:40 -0500

    add tests and refactor solution

--- day1/main.py
+++ day1/main.py
@@ -1,34 +1,40 @@
+from itertools import combinations
 import os.path
+from typing import List
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
+
+
+def solution_1(expenses: List[int]) -> int:
+    products = [
+        ex1 * ex2
+        for ex1, ex2 in combinations(expenses, 2)
+        if ex1 + ex2 == 2020
+    ]
+
+    assert len(products) == 1
+    answer = products[0]
+    return answer
+
+
+def solution_2(expenses: List[int]) -> int:
+    products = [
+        ex1 * ex2 * ex3
+        for ex1, ex2, ex3 in combinations(expenses, 3)
+        if ex1 + ex2 + ex3 == 2020
+    ]
+
+    assert len(products) == 1
+    answer = products[0]
+    return answer
 
 
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
         expenses = [int(line) for line in f.readlines()]
 
-    products = [
-        ex1 * ex2
-        for i, ex1 in enumerate(expenses)
-        for ex2 in expenses[i + 1:]
-        if ex1 + ex2 == 2020
-    ]
-
-    assert len(products) == 1
-    answer_1 = products[0]
-    print(answer_1)
-
-    products_2 = [
-        ex1 * ex2 * ex3
-        for i, ex1 in enumerate(expenses)
-        for j, ex2 in enumerate(expenses[i + 1:])
-        for ex3 in expenses[j + 1:]
-        if ex1 + ex2 + ex3 == 2020
-    ]
-
-    assert len(products_2) == 1
-    answer_2 = products_2[0]
-    print(answer_2)
+    print(solution_1(expenses))
+    print(solution_2(expenses))
 
 
 if __name__ == '__main__':
---
+++ day1/test.py
@@ -0,0 +1,19 @@
+from main import solution_1, solution_2
+
+
+EXPENSES = [
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456,
+]
+
+
+def test_solution_1() -> None:
+    assert solution_1(EXPENSES) == 514579
+
+
+def test_solution_2() -> None:
+    assert solution_2(EXPENSES) == 241861950
