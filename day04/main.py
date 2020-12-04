4747252395427ac19bfe3a06b9772fb485908c50
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-04 08:20:49 -0500

    part 1

--- day04/main.py
+++ day04/main.py
@@ -1,83 +1,36 @@
-from dataclasses import dataclass
 import os.path
-from typing import Optional, Union
+from typing import Dict, List, Tuple
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
 
-class ValidationError(Exception):
-    pass
-
-
-@dataclass
-class RawPassport:
-    birth_year: Optional[int]
-    issue_year: Optional[int]
-    expiration_year: Optional[int]
-    height: Optional[str]
-    hair_color: Optional[str]
-    eye_color: Optional[str]
-    passport_id: Optional[str]
-    country_id: Optional[str]
-
-
-@dataclass
-class Passport:
-    birth_year: int
-    issue_year: int
-    expiration_year: int
-    height: str
-    hair_color: str
-    eye_color: str
-    passport_id: str
-    country_id: Optional[str]
-
-    def __post_init__(self):
-        raise ValidationError(
-            'Passport must be initialized through validated constructor'
-        )
-
-
-
-def parse_recordss(document: str) -> List[Passport]:
+def parse_records(document: str) -> List[Dict[str, str]]:
     return [
-        parse_passport(record) 
+        parse_record(record)
         for record in document.split('\n\n')
-        if is_valid(parse_passport(record))
     ]
 
 
-def parse_record(record: str) -> RawPassport:
+def parse_record(record: str) -> Dict[str, str]:
     raw_fields = record.replace(' ', '\n').split()
-    parsed_fields = dict(parse_field(field) for field in raw_fields)
-    return RawPassport(**parsed_fields)
+
+    def split_field(field: str) -> Tuple[str, str]:
+        key, value = field.split(':', 1)
+        return key, value
+    return dict(split_field(field) for field in raw_fields)
 
 
-def parse_field(field: str) -> Tuple[str, Union[str, int]]:
-    key, value = field.split(':', 1)
-    if key == 'byr':
-        return ('birth_year': int(value))
-    elif key == 'iyr':
-        return ('issue_year' int(value))
-    elif key == 'eyr':
-        return ('expiration_year' int(value))
-    elif key == 'hgt':
-        return ('height' value)
-    elif key == 'hcl':
-        return ('hair_color' value)
-    elif key == 'ecl':
-        return ('eye_color' value)
-    elif key == 'pid':
-        return ('passport_id' value)
-    elif key == 'cid':
-        return ('country_id' value)
-    raise ValueError(f'Unknown key: {key}')
+def validate_passport(password: Dict[str, str]) -> bool:
+    required_fields = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
+    return required_fields <= password.keys()
 
 
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
-        for line in f.readlines():
-            print(line.strip())
+        passport_batch = f.read()
+
+    passports = parse_records(passport_batch)
+    print(sum(1 for passport in passports if validate_passport(passport)))
 
 
 if __name__ == "__main__":
--- day04/test.py
+++ day04/test.py
@@ -1,5 +1,30 @@
+import pytest
+
 from . import main
 
 
-def test_main() -> None:
-    main.main()
+@pytest.mark.parametrize(
+    ['record', 'is_valid'],
+
+    (
+        ("ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\n"
+         "byr:1937 iyr:2017 cid:147 hgt:183cm",
+         True),
+
+        ("iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\n"
+         "hcl:#cfa07d byr:1929",
+         False),
+
+        ("hcl:#ae17e1 iyr:2013\n"
+         "eyr:2024\n"
+         "ecl:brn pid:760753108 byr:1931\n"
+         "hgt:179cm",
+         True),
+
+        ("hcl:#cfa07d eyr:2025 pid:166559648\n"
+         "iyr:2011 ecl:brn hgt:59in",
+         False),
+    )
+)
+def test_validate_password(record: str, is_valid: bool) -> None:
+    assert main.validate_passport(main.parse_record(record)) == is_valid
