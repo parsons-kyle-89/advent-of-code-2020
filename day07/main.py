7032835876ceb57ea9968ac844df03b6f0cd7086
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-07 08:31:18 -0500

    flake and add tests

--- day07/main.py
+++ day07/main.py
@@ -2,7 +2,10 @@ from collections import defaultdict
 from dataclasses import dataclass
 from itertools import takewhile
 import os.path
-from typing import Collection, DefaultDict, Dict, Iterable, Iterator, List, Mapping, NewType, Set
+from typing import (
+    Collection, DefaultDict, Dict, Iterable, Iterator, List,
+    Mapping, NewType, Set,
+)
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
@@ -26,14 +29,19 @@ def parse_rule(raw_rule: str) -> Rule:
     bag_type = BagType(raw_bag_type)
     parsed_sub_rules = [
         parse_sub_rule(raw_sub_rule)
-        for raw_sub_rule in raw_sub_rules.split(', ')
+        for raw_sub_rule in raw_sub_rules.strip('.').split(', ')
     ]
-    sub_rules = [sub_rule for sub_rule in parsed_sub_rules if sub_rule.bag_type != BagType('other')]
+    sub_rules = [
+        sub_rule for sub_rule in parsed_sub_rules
+        if sub_rule.bag_type != BagType('other')
+    ]
     return Rule(bag_type, sub_rules)
 
 
 def parse_sub_rule(raw_sub_rule: str) -> SubRule:
-    stripped_raw_sub_rule = raw_sub_rule.strip('.').removesuffix(' bag').removesuffix(' bags')
+    stripped_raw_sub_rule = (
+        raw_sub_rule.removesuffix(' bag').removesuffix(' bags')
+    )
     raw_count, raw_bag_type = stripped_raw_sub_rule.split(' ', 1)
     bag_type = BagType(raw_bag_type)
     count = parse_count(raw_count)
@@ -46,37 +54,63 @@ def parse_count(raw_count: str) -> int:
     return int(raw_count)
 
 
-def able_to_contain_any(bag_types: Collection[BagType], rules: Iterable[Rule]) -> Set[BagType]:
-    return {rule.bag_type for rule in rules if any(bag_type in [sub_rule.bag_type for sub_rule in rule.sub_rules] for bag_type in bag_types)}
+def able_to_contain_any(
+    bag_types: Collection[BagType],
+    rules: Iterable[Rule],
+) -> Set[BagType]:
+    return {
+        rule.bag_type
+        for rule in rules
+        if any(
+            bag_type in [sub_rule.bag_type for sub_rule in rule.sub_rules]
+            for bag_type in bag_types
+        )
+    }
 
 
-def transitive_able_to_contain_any(bag_types: Collection[BagType], rules: Iterable[Rule]) -> Set[BagType]:
+def transitive_able_to_contain_any(
+    bag_types: Collection[BagType],
+    rules: Iterable[Rule],
+) -> Set[BagType]:
     containing_set = able_to_contain_any(bag_types, rules)
     while True:
-        new_containing_set = containing_set.union(able_to_contain_any(containing_set, rules))
+        new_containing_set = (
+            containing_set.union(able_to_contain_any(containing_set, rules))
+        )
         if new_containing_set == containing_set:
             break
         containing_set = new_containing_set
     return containing_set
 
 
-def contained_bags(bag_counts: Mapping[BagType, int], rules: Mapping[BagType, Rule]) -> Dict[BagType, int]:
+def contained_bags(
+    bag_counts: Mapping[BagType, int],
+    rules: Mapping[BagType, Rule],
+) -> Dict[BagType, int]:
     _contained_bags: DefaultDict[BagType, int] = defaultdict(lambda: 0)
     for bag_type, outer_bag_count in bag_counts.items():
         rule = rules[bag_type]
         for sub_rule in rule.sub_rules:
-            _contained_bags[sub_rule.bag_type] += outer_bag_count * sub_rule.count
+            _contained_bags[sub_rule.bag_type] += (
+                outer_bag_count * sub_rule.count
+            )
     return dict(_contained_bags)
 
 
-def bag_neighborhoods(bag_type: BagType, rule_map: Mapping[BagType, Rule]) -> Iterator[Dict[BagType, int]]:
+def bag_neighborhoods(
+    bag_type: BagType,
+    rule_map: Mapping[BagType, Rule],
+) -> Iterator[Dict[BagType, int]]:
     neighborhood = {bag_type: 1}
     while True:
         yield neighborhood
         neighborhood = contained_bags(neighborhood, rule_map)
 
 
-def bags_inside(bag_type: BagType, rule_map: Mapping[BagType, Rule]) -> Dict[BagType, int]:
+def bags_inside(
+    bag_type: BagType,
+    rule_map: Mapping[BagType, Rule],
+) -> Dict[BagType, int]:
     nested_bags = takewhile(bool, bag_neighborhoods(bag_type, rule_map))
     all_bags: DefaultDict[BagType, int] = defaultdict(lambda: 0)
     for nested_bag in nested_bags:
@@ -93,7 +127,11 @@ def main() -> None:
     print(len(transitive_able_to_contain_any({BagType('shiny gold')}, rules)))
 
     rule_map = {rule.bag_type: rule for rule in rules}
-    print(sum(count for count in bags_inside(BagType('shiny gold'), rule_map).values()))
+    print(
+        sum(count for count in bags_inside(
+            BagType('shiny gold'), rule_map
+        ).values())
+    )
 
 
 if __name__ == "__main__":
--- day07/test.py
+++ day07/test.py
@@ -3,3 +3,40 @@ from . import main
 
 def test_main() -> None:
     main.main()
+
+
+def test_transitive_able_to_contain_any() -> None:
+    raw_rules = """
+        light red bags contain 1 bright white bag, 2 muted yellow bags.
+        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+        bright white bags contain 1 shiny gold bag.
+        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+        faded blue bags contain no other bags.
+        dotted black bags contain no other bags.
+    """.strip()
+
+    rules = [main.parse_rule(rule.strip()) for rule in raw_rules.split('\n')]
+    actual = main.transitive_able_to_contain_any(
+        {main.BagType('shiny gold')}, rules
+    )
+    assert len(actual) == 4
+
+
+def test_bags_inside() -> None:
+    raw_rules = """
+        shiny gold bags contain 2 dark red bags.
+        dark red bags contain 2 dark orange bags.
+        dark orange bags contain 2 dark yellow bags.
+        dark yellow bags contain 2 dark green bags.
+        dark green bags contain 2 dark blue bags.
+        dark blue bags contain 2 dark violet bags.
+        dark violet bags contain no other bags.
+    """.strip()
+
+    rules = [main.parse_rule(rule.strip()) for rule in raw_rules.split('\n')]
+    rule_map = {rule.bag_type: rule for rule in rules}
+    actual = main.bags_inside(main.BagType('shiny gold'), rule_map)
+    assert sum(count for count in actual.values()) == 126
