7db27116fd38009d5353d75c79678c8b303fe821
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-09 08:29:03 -0500

    part 2 and test

--- day09/test.py
+++ day09/test.py
@@ -1,13 +1,20 @@
 from . import main
 
+EXAMPLE_NUMBERS = [
+    35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102,
+    117, 150, 182, 127, 219, 299, 277, 309, 576,
+]
+
 
 def test_main() -> None:
     main.main()
 
 
 def test_first_invalid() -> None:
-    input_numbers = [
-        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102,
-        117, 150, 182, 127, 219, 299, 277, 309, 576,
-    ]
-    assert main.first_invalid(input_numbers, 5) == 127
+    assert main.first_invalid(EXAMPLE_NUMBERS, 5) == 127
+
+
+def test_first_vulnerable_stride() -> None:
+    assert (
+        main.first_vulnerable_stride(EXAMPLE_NUMBERS, 127) == [15, 25, 47, 40]
+    )
