b0638dcfd2fdcac4be1b92c36ac9628560fc6c88
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-09 08:13:14 -0500

    add part 1 test

--- day09/test.py
+++ day09/test.py
@@ -3,3 +3,11 @@ from . import main
 
 def test_main() -> None:
     main.main()
+
+
+def test_first_invalid() -> None:
+    input_numbers = [
+        35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102,
+        117, 150, 182, 127, 219, 299, 277, 309, 576,
+    ]
+    assert main.first_invalid(input_numbers, 5) == 127
