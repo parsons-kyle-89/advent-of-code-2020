4a088aa434808e506504b583dff75e7a39c182d6
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-09 08:09:59 -0500

    part 1

--- day09/main.py
+++ day09/main.py
@@ -1,17 +1,28 @@
+from itertools import combinations
 import os.path
+from typing import List, Optional
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
 
+def first_invalid(input_numbers: List[int], lag: int) -> Optional[int]:
+    for i, a in enumerate(input_numbers):
+        if i < lag:
+            continue
+
+        local_preamble = input_numbers[(i - lag):i]
+        if a not in {x + y for x, y in combinations(local_preamble, 2)}:
+            return a
+    return None
+
+
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
-        answer_1, answer_2 = f.readlines()
+        numbers = [int(line) for line in f.readlines()]
 
-    assert answer_1 == 'hello\n'
+    answer_1 = first_invalid(numbers, 25)
+    assert answer_1 == 57195069
     print(answer_1)
-
-    assert answer_2 == 'there\n'
-    print(answer_2)
 
 
 if __name__ == "__main__":
