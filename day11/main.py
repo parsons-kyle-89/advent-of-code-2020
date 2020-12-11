e938519fd61b9013cf35343e4b2033e331e713de
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-11 08:54:27 -0500

    part 1 and refactoring

--- day11/main.py
+++ day11/main.py
@@ -1,17 +1,113 @@
+from enum import Enum
 import os.path
+from typing import Callable, Sequence, Tuple
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
 
+class State(Enum):
+    Empty = 'L'
+    Occupied = '#'
+    Floor = '.'
+
+
+class StateArrangement:
+    def __init__(self, states: Sequence[Sequence[State]]):
+        n_rows = len(states)
+        if n_rows == 0:
+            raise ValueError(f"States must be nonempty, got: {states}")
+
+        n_cols = len(states[0])
+        for i, row in enumerate(states):
+            if len(row) != n_cols:
+                raise ValueError(
+                    f"All rows must be same length, found row {i} with"
+                    f"with length {len(row)} instead of {n_cols}: {row}"
+                )
+
+        self._states = states
+        self._n_rows = n_rows
+        self._n_cols = n_cols
+
+    def __str__(self) -> str:
+        return '\n'.join(''.join(s.value for s in row) for row in self._states)
+
+    def __getitem__(self, idx: Tuple[int, int]) -> State:
+        row, col = idx
+        if (0 <= row < self._n_rows) and (0 <= col < self._n_cols):
+            return self._states[row][col]
+        return State.Empty
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, StateArrangement):
+            return NotImplemented
+        return self._states == other._states
+
+    def _next_state(
+        self,
+        neighbor_func: Callable[[int, int], int],
+        tolerance: int,
+    ) -> "StateArrangement":
+        return StateArrangement([
+            [
+                self._next_loc_state(row, col, neighbor_func, tolerance)
+                for col in range(self._n_cols)
+            ]
+            for row in range(self._n_rows)
+        ])
+
+    def next_state_adjacent(self) -> "StateArrangement":
+        return self._next_state(self._adjacent_neighbors, 4)
+
+    def _adjacent_neighbors(self, row: int, col: int) -> int:
+        return sum(
+            self[r, c] == State.Occupied
+            for r in range(row - 1, row + 2)
+            for c in range(col - 1, col + 2)
+            if not (r == row and c == col)
+        )
+
+    def _next_loc_state(
+        self,
+        row: int,
+        col: int,
+        neighbor_func: Callable[[int, int], int],
+        tolerance: int,
+    ) -> State:
+        this_state = self[row, col]
+        if this_state is State.Floor:
+            return State.Floor
+        num_neighbors = neighbor_func(row, col)
+        if num_neighbors == 0 and this_state is State.Empty:
+            return State.Occupied
+        if num_neighbors >= tolerance and this_state == State.Occupied:
+            return State.Empty
+        return this_state
+
+    def num_in_state(self, state: State) -> int:
+        return sum(s is state for row in self._states for s in row)
+
+    @classmethod
+    def from_raw(cls, raw_states: str) -> "StateArrangement":
+        return cls([
+            [State(raw_state) for raw_state in raw_row]
+            for raw_row in raw_states.splitlines()
+        ])
+
+
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
-        answer_1, answer_2 = f.readlines()
+        arrangement = StateArrangement.from_raw(f.read())
 
-    assert answer_1 == 'hello\n'
+    while True:
+        next_arrangement = arrangement.next_state_adjacent()
+        if arrangement == next_arrangement:
+            break
+        arrangement = next_arrangement
+
+    answer_1 = arrangement.num_in_state(State.Occupied)
+    assert answer_1 == 2277
     print(answer_1)
-
-    assert answer_2 == 'there\n'
-    print(answer_2)
 
 
 if __name__ == "__main__":
--- day11/test.py
+++ day11/test.py
@@ -1,5 +1,91 @@
+import pytest
+
 from . import main
 
 
+@pytest.mark.slow
 def test_main() -> None:
     main.main()
+
+
+def test_next_state() -> None:
+    raw_states = [
+        (
+            "L.LL.LL.LL\n"
+            "LLLLLLL.LL\n"
+            "L.L.L..L..\n"
+            "LLLL.LL.LL\n"
+            "L.LL.LL.LL\n"
+            "L.LLLLL.LL\n"
+            "..L.L.....\n"
+            "LLLLLLLLLL\n"
+            "L.LLLLLL.L\n"
+            "L.LLLLL.LL\n"
+        ),
+        (
+            "#.##.##.##\n"
+            "#######.##\n"
+            "#.#.#..#..\n"
+            "####.##.##\n"
+            "#.##.##.##\n"
+            "#.#####.##\n"
+            "..#.#.....\n"
+            "##########\n"
+            "#.######.#\n"
+            "#.#####.##\n"
+        ),
+        (
+            "#.LL.L#.##\n"
+            "#LLLLLL.L#\n"
+            "L.L.L..L..\n"
+            "#LLL.LL.L#\n"
+            "#.LL.LL.LL\n"
+            "#.LLLL#.##\n"
+            "..L.L.....\n"
+            "#LLLLLLLL#\n"
+            "#.LLLLLL.L\n"
+            "#.#LLLL.##\n"
+        ),
+        (
+            "#.##.L#.##\n"
+            "#L###LL.L#\n"
+            "L.#.#..#..\n"
+            "#L##.##.L#\n"
+            "#.##.LL.LL\n"
+            "#.###L#.##\n"
+            "..#.#.....\n"
+            "#L######L#\n"
+            "#.LL###L.L\n"
+            "#.#L###.##\n"
+        ),
+        (
+            "#.#L.L#.##\n"
+            "#LLL#LL.L#\n"
+            "L.L.L..#..\n"
+            "#LLL.##.L#\n"
+            "#.LL.LL.LL\n"
+            "#.LL#L#.##\n"
+            "..L.L.....\n"
+            "#L#LLLL#L#\n"
+            "#.LLLLLL.L\n"
+            "#.#L#L#.##\n"
+        ),
+        (
+            "#.#L.L#.##\n"
+            "#LLL#LL.L#\n"
+            "L.#.L..#..\n"
+            "#L##.##.L#\n"
+            "#.#L.LL.LL\n"
+            "#.#L#L#.##\n"
+            "..L.L.....\n"
+            "#L#L##L#L#\n"
+            "#.LLLLLL.L\n"
+            "#.#L#L#.##\n"
+        )
+    ]
+
+    states = [main.StateArrangement.from_raw(raw) for raw in raw_states]
+    for st_init, st_next in zip(states[:-1], states[1:]):
+        assert st_init.next_state_adjacent() == st_next
+    assert states[-1].next_state_adjacent() == states[-1]
+    assert states[-1].num_in_state(main.State.Occupied) == 37
