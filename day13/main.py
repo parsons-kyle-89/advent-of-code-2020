56b1740063a9ce4c2b98bd69fcccc7fd8bf42516
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-13 00:33:06 -0500

    part 1 solution and test

--- day13/main.py
+++ day13/main.py
@@ -1,17 +1,27 @@
+from operator import itemgetter
 import os.path
+from typing import Iterable, Tuple
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
 
+def next_departure_bus_and_wait(
+    arrival: int, buses: Iterable[int]
+) -> Tuple[int, int]:
+    return min(
+        ((bus, -arrival % bus) for bus in buses), key=itemgetter(1)
+    )
+
+
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
-        answer_1, answer_2 = f.readlines()
+        arrival = int(f.readline().strip())
+        buses = [int(b) for b in f.readline().split(',') if b != 'x']
 
-    assert answer_1 == 'hello\n'
+    bus, wait = next_departure_bus_and_wait(arrival, buses)
+    answer_1 = bus * wait
+    assert answer_1 == 2545
     print(answer_1)
-
-    assert answer_2 == 'there\n'
-    print(answer_2)
 
 
 if __name__ == "__main__":
--- day13/test.py
+++ day13/test.py
@@ -3,3 +3,9 @@ from . import main
 
 def test_main() -> None:
     main.main()
+
+
+def test_next_departure_bus_and_wait() -> None:
+    arrival = 939
+    buses = [7, 13, 59, 31, 19]
+    assert main.next_departure_bus_and_wait(arrival, buses) == (59, 5)
