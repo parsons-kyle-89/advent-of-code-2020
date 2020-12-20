26e35782545249bba54129b779a62e0bc3da08f9
parsons-kyle-89 <31140928+parsons-kyle-89@users.noreply.github.com>
2020-12-19 21:22:35 -0500

    Day 19 (#26)
    * add some safety around the day 18 eval
    
    * day 19 template
    
    * add input
    
    * stash work so I can trash it
    
    * part 2 and tests
    
    * totally refactor solution
    
    * final refactoring

---
+++ day19/main.py
@@ -0,0 +1,107 @@
+import os.path
+from itertools import product
+import re
+from typing import Dict, Iterator, Set, Tuple
+
+SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
+
+
+def parse_rule(raw_rule: str) -> Tuple[int, Set[str]]:
+    ref, pipe_delim_parts = raw_rule.split(': ')
+    parts = {
+        (
+            re.sub(r'(\d+)', lambda m: f'{{{m.group(0)}}}', part)
+            .replace(' ', '')
+            .replace('"', '')
+        )
+        for part in pipe_delim_parts.split(' | ')
+    }
+    return int(ref), parts
+
+
+def fully_simplify_rule_dict(
+    rule_dict: Dict[int, Set[str]]
+) -> Dict[int, Set[str]]:
+    while any(
+        any('{' in option for option in rule)
+        for rule in rule_dict.values()
+    ):
+        rule_dict = simplify_rule_dict(rule_dict)
+    return rule_dict
+
+
+def simplify_rule_dict(rule_dict: Dict[int, Set[str]]) -> Dict[int, Set[str]]:
+    return {
+        ref: simplify_rule(rule, rule_dict)
+        for ref, rule in rule_dict.items()
+    }
+
+
+def simplify_rule(rule: Set[str], rule_dict: Dict[int, Set[str]]) -> Set[str]:
+    no_options: Set[str] = set()
+    return no_options.union(*(
+        deref_option(option, rule_dict)
+        for option in rule
+    ))
+
+
+def deref_option(option: str, rule_dict: Dict[int, Set[str]]) -> Set[str]:
+    refs = [int(match) for match in re.findall(r'{(\d+)}', option)]
+    anonymous_option = re.sub(r'\d+', '', option)
+    return {
+        anonymous_option.format(*derefs) for derefs in
+        product(*(rule_dict[ref] for ref in refs))
+    }
+
+
+def pref_suff_strip_equally(
+    string: str,
+    pref_rule: Set[str],
+    suff_rule: Set[str],
+) -> Iterator[str]:
+    for pref_option, suff_option in product(pref_rule, suff_rule):
+        stripped = string.removeprefix(pref_option).removesuffix(suff_option)
+        if len(stripped) == len(string) - len(pref_option) - len(suff_option):
+            yield stripped
+            yield from pref_suff_strip_equally(stripped, pref_rule, suff_rule)
+
+
+def matches_special_case(
+    example: str,
+    rule_42: Set[str],
+    rule_31: Set[str],
+) -> int:
+    return any(
+        rule_8_and_11_candidate == ''
+        for rule_8_candidate in pref_suff_strip_equally(example, rule_42, {''})
+        for rule_8_and_11_candidate in pref_suff_strip_equally(
+            rule_8_candidate, rule_42, rule_31
+        )
+    )
+
+
+def main() -> None:
+    with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
+        block_rules, block_examples = f.read().split('\n\n')
+
+    examples = block_examples.splitlines()
+    rule_dict = dict(parse_rule(rule) for rule in block_rules.splitlines())
+    simple_rule_dict = fully_simplify_rule_dict(rule_dict)
+
+    rule_0 = simple_rule_dict[0]
+    answer_1 = sum(1 for example in examples if example in rule_0)
+    assert answer_1 == 176
+    print(answer_1)
+
+    rule_42 = simple_rule_dict[42]
+    rule_31 = simple_rule_dict[31]
+    answer_2 = sum(
+        1 for example in examples
+        if matches_special_case(example, rule_42, rule_31)
+    )
+    assert answer_2 == 352
+    print(answer_2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ day19/test.py
@@ -0,0 +1,132 @@
+from typing import Iterator, Set, Tuple
+
+import pytest
+
+from . import main
+
+
+@pytest.mark.slow
+def test_main() -> None:
+    main.main()
+
+
+@pytest.mark.parametrize(
+    ['rule', 'expected'],
+    (
+        ('77: 30 112 | 20 13', (77, {'{30}{112}', '{20}{13}'})),
+        ('77: "a"', (77, {'a'})),
+        ('77: "a" | 20 13', (77, {'a', '{20}{13}'})),
+        ('77: "a" 13 | 20 13', (77, {'a{13}', '{20}{13}'})),
+    )
+)
+def test_parse_rule(rule: str, expected: Tuple[int, Set[str]]) -> None:
+    assert main.parse_rule(rule) == expected
+
+
+@pytest.mark.parametrize(
+    ["example", "expected"],
+    (
+        ('ababbb', True),
+        ('bababa', False),
+        ('abbbab', True),
+        ('aaabbb', False),
+        ('aaaabbb', False),
+    )
+)
+def test_matches_rule_0(
+    example: str,
+    expected: bool,
+    flat_rule_0: Set[str],
+) -> None:
+    assert (example in flat_rule_0) == expected
+
+
+@pytest.mark.parametrize(
+    ["example", "expected"],
+    (
+        ('abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa', False),
+        ('bbabbbbaabaabba', True),
+        ('babbbbaabbbbbabbbbbbaabaaabaaa', True),
+        ('aaabbbbbbaaaabaababaabababbabaaabbababababaaa', True),
+        ('bbbbbbbaaaabbbbaaabbabaaa', True),
+        ('bbbababbbbaaaaaaaabbababaaababaabab', True),
+        ('ababaaaaaabaaab', True),
+        ('ababaaaaabbbaba', True),
+        ('baabbaaaabbaaaababbaababb', True),
+        ('abbbbabbbbaaaababbbbbbaaaababb', True),
+        ('aaaaabbaabaaaaababaa', True),
+        ('aaaabbaaaabbaaa', False),
+        ('aaaabbaabbaaaaaaabbbabbbaaabbaabaaa', True),
+        ('babaaabbbaaabaababbaabababaaab', False),
+        ('aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba', True),
+    )
+)
+def test_special_case_answer_2(
+    example: str,
+    expected: bool,
+    flat_rules_42_and_31: Tuple[Set[str], Set[str]],
+) -> None:
+    flat_rule_42, flat_rule_31 = flat_rules_42_and_31
+    assert (
+        main.matches_special_case(example, flat_rule_42, flat_rule_31) ==
+        expected
+    )
+
+
+@pytest.fixture(scope='module')
+def flat_rule_0() -> Iterator[Set[str]]:
+    block_rules = (
+        '0: 4 1 5\n'
+        '1: 2 3 | 3 2\n'
+        '2: 4 4 | 5 5\n'
+        '3: 4 5 | 5 4\n'
+        '4: "a"\n'
+        '5: "b"\n'
+    )
+    rule_dict = dict(
+        main.parse_rule(rule) for rule in block_rules.splitlines()
+    )
+    simple_rule_dict = main.fully_simplify_rule_dict(rule_dict)
+    yield simple_rule_dict[0]
+
+
+@pytest.fixture(scope='module')
+def flat_rules_42_and_31() -> Iterator[Tuple[Set[str], Set[str]]]:
+    block_rules = (
+        '42: 9 14 | 10 1\n'
+        '9: 14 27 | 1 26\n'
+        '10: 23 14 | 28 1\n'
+        '1: "a"\n'
+        '11: 42 31\n'
+        '5: 1 14 | 15 1\n'
+        '19: 14 1 | 14 14\n'
+        '12: 24 14 | 19 1\n'
+        '16: 15 1 | 14 14\n'
+        '31: 14 17 | 1 13\n'
+        '6: 14 14 | 1 14\n'
+        '2: 1 24 | 14 4\n'
+        '0: 8 11\n'
+        '13: 14 3 | 1 12\n'
+        '15: 1 | 14\n'
+        '17: 14 2 | 1 7\n'
+        '23: 25 1 | 22 14\n'
+        '28: 16 1\n'
+        '4: 1 1\n'
+        '20: 14 14 | 1 15\n'
+        '3: 5 14 | 16 1\n'
+        '27: 1 6 | 14 18\n'
+        '14: "b"\n'
+        '21: 14 1 | 1 14\n'
+        '25: 1 1 | 1 14\n'
+        '22: 14 14\n'
+        '8: 42\n'
+        '26: 14 22 | 1 20\n'
+        '18: 15 15\n'
+        '7: 14 5 | 1 21\n'
+        '24: 14 1\n'
+    )
+    rule_dict = dict(
+        main.parse_rule(rule) for rule in block_rules.splitlines()
+    )
+    simple_rule_dict = main.fully_simplify_rule_dict(rule_dict)
+    yield simple_rule_dict[42], simple_rule_dict[31]
