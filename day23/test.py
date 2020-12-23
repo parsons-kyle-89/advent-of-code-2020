c0f7f9e300be665ae04c3a81d3c16373ed9c6c7e
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-23 08:06:08 -0500

    parts 1 and 2

--- day23/test.py
+++ day23/test.py
@@ -1,5 +1,23 @@
+from typing import List
+
+import pytest
+
 from . import main
 
 
+@pytest.mark.slow
 def test_main() -> None:
     main.main()
+
+
+@pytest.mark.parametrize(
+    ["num_moves", "expected"],
+    (
+        (10, [9, 2, 6, 5, 8, 3, 7, 4, 1]),
+        (100, [6, 7, 3, 8, 4, 5, 2, 9, 1]),
+        (10_000_000, [1, 2, 3]),
+    )
+)
+def test_moves(num_moves: int, expected: List[int]) -> None:
+    order = [3, 8, 9, 1, 2, 5, 4, 6, 7]
+    assert main.cup_game(order, num_moves) == expected
