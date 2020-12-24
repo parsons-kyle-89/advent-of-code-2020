47cd5c0a662d982b08a721180d4a9a49d1e524f9
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-24 09:01:40 -0500

    part 1

--- day24/main.py
+++ day24/main.py
@@ -1,17 +1,64 @@
+from collections import defaultdict
+from dataclasses import dataclass
 import os.path
+import re
+from typing import Dict, Iterable, List, MutableMapping
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
 
+@dataclass(frozen=True)
+class Vec:
+    __slots__ = ('q', 'r')
+    q: int
+    r: int
+
+    def __add__(self, other: "Vec") -> "Vec":
+        return Vec(self.q + other.q, self.r + other.r)
+
+
+def parse_steps(raw_steps: str) -> List[Vec]:
+    delimited_steps = re.findall('ne|nw|se|sw|e|w', raw_steps)
+    return [parse_step(raw_step) for raw_step in delimited_steps]
+
+
+def parse_step(raw_step: str) -> Vec:
+    if raw_step == 'ne':
+        vec = Vec(1, -1)
+    elif raw_step == 'nw':
+        vec = Vec(0, -1)
+    elif raw_step == 'se':
+        vec = Vec(0, 1)
+    elif raw_step == 'sw':
+        vec = Vec(-1, 1)
+    elif raw_step == 'e':
+        vec = Vec(1, 0)
+    elif raw_step == 'w':
+        vec = Vec(-1, 0)
+    else:
+        raise ValueError(f"cannot convert str {raw_step} into Step")
+    return vec
+
+
+def reduce_steps(steps: Iterable[Vec]) -> Vec:
+    return sum(steps, Vec(0, 0))
+
+
+def tile_parities_from_steps(tiles: Iterable[Vec]) -> Dict[Vec, int]:
+    tile_parities: MutableMapping[Vec, int] = defaultdict(lambda: 0)
+    for vec in tiles:
+        tile_parities[vec] = (tile_parities[vec] + 1) % 2
+    return dict(tile_parities)
+
+
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
-        answer_1, answer_2 = f.readlines()
+        steps_list = [parse_steps(raw_steps) for raw_steps in f.readlines()]
 
-    assert answer_1 == 'hello\n'
+    tiles = [reduce_steps(steps) for steps in steps_list]
+    tile_parities = tile_parities_from_steps(tiles)
+    answer_1 = sum(tile_parities.values())
     print(answer_1)
-
-    assert answer_2 == 'there\n'
-    print(answer_2)
 
 
 if __name__ == "__main__":
--- day24/test.py
+++ day24/test.py
@@ -3,3 +3,39 @@ from . import main
 
 def test_main() -> None:
     main.main()
+
+
+def test_parse_tiles() -> None:
+    raw_steps = 'esenee'
+    steps = main.parse_steps(raw_steps)
+    tile = main.reduce_steps(steps)
+    assert tile == main.Vec(3, 0)
+
+
+def test_tile_parities() -> None:
+    raw_tiles = [
+        'sesenwnenenewseeswwswswwnenewsewsw',
+        'neeenesenwnwwswnenewnwwsewnenwseswesw',
+        'seswneswswsenwwnwse',
+        'nwnwneseeswswnenewneswwnewseswneseene',
+        'swweswneswnenwsewnwneneseenw',
+        'eesenwseswswnenwswnwnwsewwnwsene',
+        'sewnenenenesenwsewnenwwwse',
+        'wenwwweseeeweswwwnwwe',
+        'wsweesenenewnwwnwsenewsenwwsesesenwne',
+        'neeswseenwwswnwswswnw',
+        'nenwswwsewswnenenewsenwsenwnesesenew',
+        'enewnwewneswsewnwswenweswnenwsenwsw',
+        'sweneswneswneneenwnewenewwneswswnese',
+        'swwesenesewenwneswnwwneseswwne',
+        'enesenwswwswneneswsenwnewswseenwsese',
+        'wnwnesenesenenwwnenwsewesewsesesew',
+        'nenewswnwewswnenesenwnesewesw',
+        'eneswnwswnwsenenwnwnwwseeswneewsenese',
+        'neswnwewnwnwseenwseesewsenwsweewe',
+        'wseweeenwnesenwwwswnew',
+    ]
+    steps_list = [main.parse_steps(raw_steps) for raw_steps in raw_tiles]
+    tiles = [main.reduce_steps(steps) for steps in steps_list]
+    tile_parities = main.tile_parities_from_steps(tiles)
+    assert sum(tile_parities.values()) == 10
