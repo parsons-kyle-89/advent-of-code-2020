9cb75b240e29b3dc09e424c0d04d63d31c2f127d
Kyle Parsons <parsons.kyle.89@gmail.com>
2020-12-24 09:56:03 -0500

    part 2

--- day24/main.py
+++ day24/main.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 from dataclasses import dataclass
+from enum import auto, Enum
 import os.path
 import re
-from typing import Dict, Iterable, List, MutableMapping
+from typing import DefaultDict, Iterable, Iterator, List, NoReturn, Set
 
 SCRIPT_DIR = os.path.dirname(os.path.relpath(__file__))
 
@@ -15,6 +16,13 @@ class Vec:
 
     def __add__(self, other: "Vec") -> "Vec":
         return Vec(self.q + other.q, self.r + other.r)
+
+    @staticmethod
+    def cardinals() -> "Iterator[Vec]":
+        yield from iter([
+            Vec(0, -1), Vec(1, -1), Vec(1, 0),
+            Vec(0, 1), Vec(-1, 1), Vec(-1, 0),
+        ])
 
 
 def parse_steps(raw_steps: str) -> List[Vec]:
@@ -44,21 +52,77 @@ def reduce_steps(steps: Iterable[Vec]) -> Vec:
     return sum(steps, Vec(0, 0))
 
 
-def tile_parities_from_steps(tiles: Iterable[Vec]) -> Dict[Vec, int]:
-    tile_parities: MutableMapping[Vec, int] = defaultdict(lambda: 0)
+class Tile(Enum):
+    WHITE = auto()
+    BLACK = auto()
+
+
+TileArr = DefaultDict[Vec, Tile]
+
+
+def tiles_from_steps(tiles: Iterable[Vec]) -> TileArr:
+    tile_parities: DefaultDict[Vec, int] = defaultdict(lambda: 0)
     for vec in tiles:
         tile_parities[vec] = (tile_parities[vec] + 1) % 2
-    return dict(tile_parities)
+    return defaultdict(lambda: Tile.WHITE, {
+        vec: Tile.WHITE if parity == 0 else Tile.BLACK
+        for vec, parity in tile_parities.items()
+    })
+
+
+def count_black_tiles(tiles: TileArr) -> int:
+    return sum(1 for tile in tiles.values() if tile is Tile.BLACK)
+
+
+def next_arrangement(tile_arr: TileArr) -> TileArr:
+    shifts = [
+        defaultdict(lambda: Tile.WHITE, {
+            vec + card: tile for vec, tile in tile_arr.items()
+        })
+        for card in Vec.cardinals()
+    ]
+    no_vecs: Set[Vec] = set()
+    black_neighbor_counts = defaultdict(lambda: 0, {
+        vec: [shift[vec] for shift in shifts].count(Tile.BLACK)
+        for vec in no_vecs.union(*shifts)
+    })
+    return defaultdict(lambda: Tile.WHITE, {
+        vec: next_tile(tile_arr[vec], black_neighbor_counts[vec])
+        for vec in no_vecs.union(tile_arr, black_neighbor_counts)
+    })
+
+
+def next_tile(tile: Tile, black_neighbor_count: int) -> Tile:
+    if tile is Tile.BLACK:
+        if black_neighbor_count in [1, 2]:
+            return Tile.BLACK
+        else:
+            return Tile.WHITE
+    elif tile is Tile.WHITE:
+        if black_neighbor_count == 2:
+            return Tile.BLACK
+        else:
+            return Tile.WHITE
+    absurd: NoReturn = tile
+    raise ValueError(f"found absurd with value {absurd}")
 
 
 def main() -> None:
     with open(f'{SCRIPT_DIR}/input.txt', 'r') as f:
         steps_list = [parse_steps(raw_steps) for raw_steps in f.readlines()]
 
-    tiles = [reduce_steps(steps) for steps in steps_list]
-    tile_parities = tile_parities_from_steps(tiles)
-    answer_1 = sum(tile_parities.values())
+    duped_tiles = [reduce_steps(steps) for steps in steps_list]
+    tiles = tiles_from_steps(duped_tiles)
+
+    answer_1 = count_black_tiles(tiles)
+    assert answer_1 == 232
     print(answer_1)
+
+    for _ in range(100):
+        tiles = next_arrangement(tiles)
+    answer_2 = count_black_tiles(tiles)
+    assert answer_2 == 3519
+    print(answer_2)
 
 
 if __name__ == "__main__":
--- day24/test.py
+++ day24/test.py
@@ -1,6 +1,11 @@
+from typing import DefaultDict, Generator
+
+import pytest
+
 from . import main
 
 
+@pytest.mark.slow
 def test_main() -> None:
     main.main()
 
@@ -36,6 +41,61 @@ def test_tile_parities() -> None:
         'wseweeenwnesenwwwswnew',
     ]
     steps_list = [main.parse_steps(raw_steps) for raw_steps in raw_tiles]
-    tiles = [main.reduce_steps(steps) for steps in steps_list]
-    tile_parities = main.tile_parities_from_steps(tiles)
-    assert sum(tile_parities.values()) == 10
+    duped_tiles = [main.reduce_steps(steps) for steps in steps_list]
+    tiles = main.tiles_from_steps(duped_tiles)
+    assert main.count_black_tiles(tiles) == 10
+
+
+@pytest.mark.parametrize(
+    ["days", "expected_count"],
+    (
+        (1, 15),
+        (2, 12),
+        (3, 25),
+        (4, 14),
+        (5, 23),
+        (6, 28),
+        (7, 41),
+        (8, 37),
+        (9, 49),
+        (10, 37),
+    )
+)
+def test_black_tiles_by_day(
+    initial_arr: DefaultDict[main.Vec, main.Tile],
+    days: int,
+    expected_count: int
+) -> None:
+    tiles = initial_arr
+    for _ in range(days):
+        tiles = main.next_arrangement(tiles)
+    assert main.count_black_tiles(tiles) == expected_count
+
+
+@pytest.fixture(scope="module")
+def initial_arr() -> Generator[DefaultDict[main.Vec, main.Tile], None, None]:
+    raw_tiles = [
+        'sesenwnenenewseeswwswswwnenewsewsw',
+        'neeenesenwnwwswnenewnwwsewnenwseswesw',
+        'seswneswswsenwwnwse',
+        'nwnwneseeswswnenewneswwnewseswneseene',
+        'swweswneswnenwsewnwneneseenw',
+        'eesenwseswswnenwswnwnwsewwnwsene',
+        'sewnenenenesenwsewnenwwwse',
+        'wenwwweseeeweswwwnwwe',
+        'wsweesenenewnwwnwsenewsenwwsesesenwne',
+        'neeswseenwwswnwswswnw',
+        'nenwswwsewswnenenewsenwsenwnesesenew',
+        'enewnwewneswsewnwswenweswnenwsenwsw',
+        'sweneswneswneneenwnewenewwneswswnese',
+        'swwesenesewenwneswnwwneseswwne',
+        'enesenwswwswneneswsenwnewswseenwsese',
+        'wnwnesenesenenwwnenwsewesewsesesew',
+        'nenewswnwewswnenesenwnesewesw',
+        'eneswnwswnwsenenwnwnwwseeswneewsenese',
+        'neswnwewnwnwseenwseesewsenwsweewe',
+        'wseweeenwnesenwwwswnew',
+    ]
+    steps_list = [main.parse_steps(raw_steps) for raw_steps in raw_tiles]
+    duped_tiles = [main.reduce_steps(steps) for steps in steps_list]
+    yield main.tiles_from_steps(duped_tiles)
